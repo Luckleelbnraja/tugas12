7b932d22941974635bfb046fcbeb4ba8a96ee2a4
Rezza Pontoh <s22210345@student.unklab.ac.id>
2024-11-05 18:27:32 +0800

    Add Home section components

---
+++ src/components/Home/CardInfo.js
@@ -0,0 +1,12 @@
+// src/components/Home/CardInfo.js
+import React from 'react';
+
+const CardInfo = () => {
+  return (
+    <div>
+      <h1>Card-Info</h1>
+    </div>
+  );
+};
+
+export default CardInfo;
